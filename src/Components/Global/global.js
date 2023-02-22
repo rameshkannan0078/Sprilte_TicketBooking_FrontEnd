f9a50a11f831fc2e8c1cda7a448cb33140420435
rameshkannan0078 <58101430+rameshkannan0078@users.noreply.github.com>
2023-02-22 19:28:38 +0530

    Update global.js

--- src/Components/Global/global.js
+++ src/Components/Global/global.js
@@ -2,9 +2,9 @@
 
 module.exports={
     globalUrl:{
-        apiUrl:"http://localhost:3001/",
-        emailUrl:"http://localhost:3002/index.php",
-        emailFileUrl:"http://localhost:3002/sendpdf.php"
+        apiUrl:"https://spritleticketbookingbackend.onrender.com/",
+        emailUrl:"https://spritleemailapi.onrender.com/index.php",
+        emailFileUrl:"https://spritleemailapi.onrender.com/sendpdf.php"
         
     }
 }
